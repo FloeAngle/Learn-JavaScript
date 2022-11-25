b848c08318cede7d8a3b2b9516a64c3e29f617f7
Harry <1055343762@qq.com>
2022-11-25 23:45:36 +0800

    world -> new world

--- temp.js
+++ temp.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hello World
 // @namespace    https://github.com/FloeAngle/Learn-JavaScript/
-// @version      0.1
+// @version      0.2
 // @description  Harry's first script
 // @author       You
 // @match        *://*

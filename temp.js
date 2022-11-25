7f20726e2fdf3d4f3d38c6dc5fb42ab80d361e03
Harry <1055343762@qq.com>
2022-11-25 23:25:41 +0800

    hello world js

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Hello World
+// @namespace    https://github.com/FloeAngle/Learn-JavaScript/
+// @version      0.1
+// @description  Harry's first script
+// @author       You
+// @match        *://*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {//一个匿名函数
+    'use strict';//严格模式
+        console.log("Hello world!")//输出
+    // Your code here...
+    }
+)();//执行名为function的匿名函数
